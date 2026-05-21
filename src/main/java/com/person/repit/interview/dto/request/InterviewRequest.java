0c0896bf906a889be1e66481beb4ebda55d51481
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-05-21 20:45:46 +0900

    feat: interview request 수정

--- src/main/java/com/person/repit/interview/dto/request/InterviewRequest.java
+++ src/main/java/com/person/repit/interview/dto/request/InterviewRequest.java
@@ -1,7 +1,5 @@
 package com.person.repit.interview.dto.request;
 
-import com.person.repit.interview.type.InterviewStatus;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,12 +12,6 @@ public class InterviewRequest {
 
     @NotNull
     private Long personaId;
-
-    @NotNull
-    private Long sessionId;
-
-    @NotBlank
-    private InterviewStatus status;
 
 
 }
