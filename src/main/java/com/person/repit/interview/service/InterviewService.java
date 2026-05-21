e216420bda050d9b9f615180492495aade216477
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-05-21 17:00:53 +0900

    feat: implement interview service interface

--- src/main/java/com/person/repit/interview/service/InterviewService.java
+++ src/main/java/com/person/repit/interview/service/InterviewService.java
@@ -1,7 +1,15 @@
 package com.person.repit.interview.service;
 
-import com.person.repit.interview.dto.response.InterviewQuestionResponse;
+
+import com.person.repit.interview.dto.request.InterviewRequest;
+import com.person.repit.interview.dto.response.InterviewResponse;
+
+import java.util.List;
 
 public interface InterviewService {
-    InterviewQuestionResponse startInterview(StartInterviewRequest request);
+    InterviewResponse createInterview(InterviewRequest request);
+    InterviewResponse getInterview(Long interviewId);
+    List<InterviewResponse> getUserInterviews(Long userId);
+    InterviewResponse completeInterview(Long interviewId);
+    InterviewResponse quitInterview(Long interviewId);
 }
