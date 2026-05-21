c5eff71bc80056b3bc3429e7e990a98cf970b099
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-05-21 20:31:53 +0900

    feat: implement interview question controller

---
+++ src/main/java/com/person/repit/interview/controller/InterviewQuestionController.java
@@ -0,0 +1,25 @@
+package com.person.repit.interview.controller;
+
+import com.person.repit.interview.dto.request.InterviewQuestionRequest;
+import com.person.repit.interview.dto.response.InterviewQuestionResponse;
+import com.person.repit.interview.service.InterviewQuestionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/interviews/questions")
+public class InterviewQuestionController {
+
+    private final InterviewQuestionService interviewQuestionService;
+
+    @PostMapping
+    public InterviewQuestionResponse createQuestion(@Valid @RequestBody InterviewQuestionRequest request) {
+        return interviewQuestionService.createQuestion(request);
+    }
+
+    public InterviewQuestionResponse getQuestion(@PathVariable Long questionId) {
+        return interviewQuestionService.getQuestion(questionId);
+    }
+}
