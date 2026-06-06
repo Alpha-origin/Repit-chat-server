ce8bc0d9502cd646e5ef1841b102a2b0958495f2
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-06-07 00:03:25 +0900

    refactor: ai 질문 service 코드 수정

--- src/main/java/com/person/repit/interview/service/impl/AiQuestionClientImpl.java
+++ src/main/java/com/person/repit/interview/service/impl/AiQuestionClientImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientException;
 
 import java.util.List;
 import java.util.Map;
