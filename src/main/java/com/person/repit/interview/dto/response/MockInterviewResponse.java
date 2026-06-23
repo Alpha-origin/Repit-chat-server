b84e5a43bd9fb54ff7bd5d80ca4a66cd37afe15e
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-06-23 23:25:23 +0900

    fix: fix response

--- src/main/java/com/person/repit/interview/dto/response/MockInterviewResponse.java
+++ src/main/java/com/person/repit/interview/dto/response/MockInterviewResponse.java
@@ -1,30 +1,39 @@
 package com.person.repit.interview.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MockInterviewResponse {
 
     private Boolean success;
     private Data data;
 
     @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Data {
         private Result result;
     }
 
     @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Result {
         private List<InterviewQuestion> interview;
     }
 
     @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class InterviewQuestion {
+
         private Integer id;
         private String category;
         private String question;
-        private String expected_answer;
+
+        @JsonProperty("expected_answer")
+        private String expectedAnswer;
     }
 }
