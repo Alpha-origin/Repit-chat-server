9b9d713a40762a674240ded56b2bca2849dad1b7
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-06-24 13:43:20 +0900

    refactor: CORS 설정 변경

--- src/main/java/com/person/repit/common/config/CorsConfig.java
+++ src/main/java/com/person/repit/common/config/CorsConfig.java
@@ -15,8 +15,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://repit-web-git-feat-45-interview-end-page-han0911s-projects.vercel.app"
+                        )
+                        .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
