ca78c37a40b27339fa0ad50d658a8e9710cd2503
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-08-20 19:45:30 +0900

    refactor : add setter

--- src/main/java/com/person/repit/interview/dto/request/ChatInterviewPrepareRequest.java
+++ src/main/java/com/person/repit/interview/dto/request/ChatInterviewPrepareRequest.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ChatInterviewPrepareRequest {
     @NotBlank
