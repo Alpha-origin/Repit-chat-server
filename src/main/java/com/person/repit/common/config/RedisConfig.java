f34a88a23f30240cd911665ed4d46125065a8a01
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-08-29 01:42:21 +0900

    refactor : 동기 RedisTemplate 설정 제거

--- src/main/java/com/person/repit/common/config/RedisConfig.java
+++ src/main/java/com/person/repit/common/config/RedisConfig.java
@@ -2,33 +2,14 @@ package com.person.repit.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-
-        RedisSerializer<Object> serializer = RedisSerializer.json();
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(serializer);
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(serializer);
-
-        template.afterPropertiesSet();
-
-        return template;
-    }
 
     @Bean
     public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(
