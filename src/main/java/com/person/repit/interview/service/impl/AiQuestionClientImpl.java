0587cecbf29ebfc6fdada3a3ce0f93824f1a9356
2-dayeon <all.kite091106@dgsw.hs.kr>
2026-08-30 03:41:25 +0900

    fix : AI 응답 JSON 형식 수정

--- src/main/java/com/person/repit/interview/service/impl/AiQuestionClientImpl.java
+++ src/main/java/com/person/repit/interview/service/impl/AiQuestionClientImpl.java
@@ -166,7 +166,7 @@ public class AiQuestionClientImpl implements AiQuestionClient {
 
                 응답 형식:
                 {
-                  "score": 꼬리질문 점수 (0~5점)
+                  "score": 꼬리질문 점수 (0~5점),
                   "required": true 또는 false,
                   "intention": "꼬리질문 의도. required=false면 null",
                   "content": "꼬리질문 본문. required=false면 null"
